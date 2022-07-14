648a2485b08baa11347d5bb46f61dbd223923cb4
LauraStabulniece <78436423+LauraStabulniece@users.noreply.github.com>
2022-07-14 11:48:42 +0300

    ngIf ngFor images

--- src/app/illustrations/illustrations.component.ts
+++ src/app/illustrations/illustrations.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+interface Illustration {
+  name: string;
+  author: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-illustrations',
@@ -6,27 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./illustrations.component.css'],
 })
 export class IllustrationsComponent implements OnInit {
-  name: string = 'Bee';
-  author: string = 'Belinda Murphy';
-  src: string =
-    'https://i.pinimg.com/originals/bd/86/67/bd8667b2ac2d9fee2aab13645d6fe3a9.jpg';
+  illustrations: Illustration[] = [
+    {
+      name: 'Bee',
+      author: 'Belinda Murphy',
+      image: '../../assets/bee.jpg',
+    },
+    {
+      name: 'Lady with the dog',
+      author: 'Beatrice Sautereau',
+      image: '../../assets/ladyWithTheDog.jpg',
+    },
+  ];
+  // name: string = 'Bee';
+  // author: string = 'Belinda Murphy';
+  // src: string =
+  //   'https://i.pinimg.com/originals/bd/86/67/bd8667b2ac2d9fee2aab13645d6fe3a9.jpg';
 
-  name2: string = 'Lady with the dog';
-  author2: string = 'Beatrice Sautereau';
-  src2: string =
-    'http://1.bp.blogspot.com/-o-AKrWfiZrM/UFi2tGDC_7I/AAAAAAAAL0I/EBvaNwlQUKI/s1600/image-id21-128.jpg';
+  // name2: string = 'Lady with the dog';
+  // author2: string = 'Beatrice Sautereau';
+  // src2: string =
+  //   'http://1.bp.blogspot.com/-o-AKrWfiZrM/UFi2tGDC_7I/AAAAAAAAL0I/EBvaNwlQUKI/s1600/image-id21-128.jpg';
 
-  isDisabled: boolean = false;
-
+  isShowing: boolean = true;
+  // isDisabled: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
+  // toggleIllustrations() {
+  //   this.isShowing = !this.isShowing;
+  // }
 
-  handleClick() {
-    this.isDisabled = true;
-  }
+  // handleClick() {
+  //   this.isDisabled = true;
+  // }
 
-  myName: string = '';
+  // myName: string = '';
   // handleInput(event: any) {
   //   this.myName = event.target.value;
   // }
